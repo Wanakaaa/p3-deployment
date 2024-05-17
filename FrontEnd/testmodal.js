26562753d22f77b2fdf6824eb1fa807217ae1835
Sarah Colin <srh.colin@gmail.com>
2024-05-17 15:56:55 +0200

    modal

--- FrontEnd/testmodal.js
+++ FrontEnd/testmodal.js
@@ -52,26 +52,25 @@ function createModal() {
     container.innerHTML = modal;
     portfolio.appendChild(container)
 
-    // getWorksModal()
+    getWorksModal()
 }
 
 
 // Récupérer toutes les travaux de l'API
 
-// function getWorksModal(){
-// fetch("http://localhost:5678/api/works")
-//     .then((response) => response.json())
-//     .then ((works) => {
-//         console.log(works)
-//         updateGalleryModal(works)
-//     })
-// }
+function getWorksModal(){
+fetch("http://localhost:5678/api/works")
+    .then((response) => response.json())
+    .then ((works) => {
+        console.log(works)
+        updateGalleryModal(works)
+    })
+}
 
 function updateGalleryModal(works) {
     const galleryModal = document.querySelector('.galleryModal');
     if (galleryModal) {
         galleryModal.innerHTML = "";
-        console.log(works);
         works.forEach((work) => {
         const workFigure = document.createElement("figure");
         workFigure.innerHTML = `
@@ -85,71 +84,72 @@ function updateGalleryModal(works) {
 }
 
 
-
-let categoriesGlobal = [];
-fetch("http://localhost:5678/api/categories")
-    .then((response) => response.json())
-    .then((categories) => {
-        updateBtn(categories);
-
-        categoriesGlobal = categories;
-        // console.log(categoriesGlobal);
-    });
-
-let worksGlobal = [];
-fetch("http://localhost:5678/api/works")
-    .then((response) => response.json())
-    .then((works) => {
-        updateGallery(works);
-        updateGalleryModal(works)
-        worksGlobal = works;
-    });
-
-
-function updateBtn(categories) {
-    const btnTous = document.createElement("button");
-    btnTous.innerText = "Tous";
-    btnTous.id = `btnTous`;
-    divBtn.appendChild(btnTous);
-    btnTous.addEventListener('click', () => {
-        updateGallery(worksGlobal)
-    })
-
-    categories.forEach((categorie) => {
-        const btn = document.createElement("button");
-        btn.innerText = categorie.name;
-        btn.id = `btn${categorie.name}`;
-        divBtn.appendChild(btn);
-        btn.addEventListener('click', () => {
-            let filteredWorks = filterCategory(worksGlobal, categorie.id);
-            updateGallery(filteredWorks)
-        })
-    });
-}
-
-
-// Afficher les données récupérée via l'API
-
-// 1. Définir gallery (en haut)
-// afficher tous les éléments au chargement de la page
-
-function updateGallery(works) {
-    gallery.innerHTML = "";
-    works.forEach((work) => {
-        const workFigure = document.createElement("figure");
-        workFigure.innerHTML = `
-        <img src = "${work.imageUrl}" alt="${work.title}">
-        <figcaption>${work.title} </figcaption>
-        `;
-        gallery.appendChild(workFigure);
-    });
-}
+// Afficher les travaux de l'API 
 
 
 
-function filterCategory(array, categorieId) {
-    const categoryList = array.filter((work) => work.categoryId == categorieId);
-    return categoryList;
-}
+// let categoriesGlobal = [];
+// fetch("http://localhost:5678/api/categories")
+//     .then((response) => response.json())
+//     .then((categories) => {
+//         updateBtn(categories);
 
-// Modale ? 
+//         categoriesGlobal = categories;
+//         console.log(categoriesGlobal);
+//     });
+
+// let worksGlobal = [];
+// fetch("http://localhost:5678/api/works")
+//     .then((response) => response.json())
+//     .then((works) => {
+//         updateGallery(works);
+//         worksGlobal = works;
+//     });
+
+
+// function updateBtn(categories) {
+//     const btnTous = document.createElement("button");
+//     btnTous.innerText = "Tous";
+//     btnTous.id = `btnTous`;
+//     divBtn.appendChild(btnTous);
+//     btnTous.addEventListener('click', () => {
+//         updateGallery(worksGlobal)
+//     })
+
+//     categories.forEach((categorie) => {
+//         const btn = document.createElement("button");
+//         btn.innerText = categorie.name;
+//         btn.id = `btn${categorie.name}`;
+//         divBtn.appendChild(btn);
+//         btn.addEventListener('click', () => {
+//             let filteredWorks = filterCategory(worksGlobal, categorie.id);
+//             updateGallery(filteredWorks)
+//         })
+//     });
+// }
+
+
+// // Afficher les données récupérée via l'API
+
+// // 1. Définir gallery (en haut)
+// // afficher tous les éléments au chargement de la page
+
+// function updateGallery(works) {
+//     gallery.innerHTML = "";
+//     works.forEach((work) => {
+//         const workFigure = document.createElement("figure");
+//         workFigure.innerHTML = `
+//         <img src = "${work.imageUrl}" alt="${work.title}">
+//         <figcaption>${work.title} </figcaption>
+//         `;
+//         gallery.appendChild(workFigure);
+//     });
+// }
+
+
+
+// function filterCategory(array, categorieId) {
+//     const categoryList = array.filter((work) => work.categoryId == categorieId);
+//     return categoryList;
+// }
+
